3f9ddc49da85a67cd9feec897736dbe9b4860bef
Lunny Xiao <xiaolunwen@gmail.com>
2016-10-09 10:35:20 +0800

    resolved #1

--- rbac.go
+++ rbac.go
@@ -2,6 +2,7 @@ package rbac
 
 import (
 	"reflect"
+	"strconv"
 	"sync"
 
 	"github.com/lunny/tango"
@@ -62,13 +63,85 @@ func (m *Manager) SetRBACRole(roles ...string) {
 	sess.Set(m.opt.RoleSessionKey, roles)
 }
 
+// lookup search the tag
+func lookup(tag, key string) (string, bool, bool) {
+	// When modifying this code, also update the validateStructTag code
+	// in golang.org/x/tools/cmd/vet/structtag.go.
+	var hasColon bool
+	for tag != "" {
+		// Skip leading space.
+		i := 0
+		for i < len(tag) && tag[i] == ' ' {
+			i++
+		}
+
+		tag = tag[i:]
+		if tag == "" {
+			break
+		}
+
+		// Scan to colon. A space, a quote or a control character is a syntax error.
+		// Strictly speaking, control chars include the range [0x7f, 0x9f], not just
+		// [0x00, 0x1f], but in practice, we ignore the multi-byte control characters
+		// as it is simpler to inspect the tag's bytes than the tag's runes.
+		i = 0
+		for i < len(tag) && tag[i] > ' ' && tag[i] != ':' && tag[i] != '"' && tag[i] != 0x7f {
+			i++
+		}
+
+		if i == 0 || i+1 >= len(tag) || tag[i] != ':' || tag[i+1] != '"' {
+			break
+		}
+
+		if tag[i] == ':' && tag[i+1] == '"' {
+			hasColon = true
+		}
+
+		name := string(tag[:i])
+		tag = tag[i+1:]
+
+		// Scan quoted string to find value.
+		i = 1
+		for i < len(tag) && tag[i] != '"' {
+			if tag[i] == '\\' {
+				i++
+			}
+			i++
+		}
+		if i >= len(tag) {
+			break
+		}
+		qvalue := string(tag[:i+1])
+		tag = tag[i+1:]
+
+		if key == name {
+			value, err := strconv.Unquote(qvalue)
+			if err != nil {
+				break
+			}
+			return value, true, hasColon
+		}
+	}
+	return "", false, hasColon
+}
+
 var _ manager = &Manager{}
+
+type PermTager interface {
+	PermTag() string
+}
+
+type RolesTager interface {
+	RolesTag() string
+}
 
 // RBAC return a rbac handler.
 func RBAC(rbac *gorbac.RBAC, sessions *session.Sessions, opts ...Options) tango.HandlerFunc {
 	opt := prepareOptions(opts)
 	var cachePerms = make(map[reflect.Value]string)
 	var cachePermsLock sync.Mutex
+	var cacheRoles = make(map[reflect.Value]string)
+	var cacheRolesLock sync.Mutex
 
 	return func(ctx *tango.Context) {
 		if action := ctx.Action(); action != nil {
@@ -77,43 +150,83 @@ func RBAC(rbac *gorbac.RBAC, sessions *session.Sessions, opts ...Options) tango.
 			}
 
 			var permTag string
-			var ok bool
 			actionValue := ctx.ActionValue()
 
-			cachePermsLock.Lock()
-			if permTag, ok = cachePerms[actionValue]; !ok {
-				permTag = ctx.ActionTag("Perm")
-				if len(permTag) > 0 {
-					cachePerms[actionValue] = permTag
+			if pt, ok := action.(PermTager); ok {
+				permTag = pt.PermTag()
+			} else {
+				cachePermsLock.Lock()
+				if permTag, ok = cachePerms[actionValue]; !ok {
+					permTag = ctx.ActionTag("Perm")
+					if len(permTag) > 0 {
+						cachePerms[actionValue] = permTag
+					}
 				}
+				cachePermsLock.Unlock()
 			}
-			cachePermsLock.Unlock()
 
-			if permTag != "" {
-				pA := gorbac.NewStdPermission(permTag)
+			if len(permTag) > 0 {
 				roles, ok := sessions.Session(ctx.Req(), ctx.ResponseWriter).Get(opt.RoleSessionKey).([]string)
 				if !ok {
 					opt.OnNoPerm(ctx)
 					return
 				}
+				tag, ok, hasColon := lookup(permTag, ctx.Req().Method)
+				if hasColon {
+					if !ok {
+						opt.OnNoPerm(ctx)
+						return
+					}
+				} else {
+					tag = permTag
+				}
+
+				var pA gorbac.Permission
+				pA = gorbac.NewStdPermission(tag)
 				for _, role := range roles {
 					if rbac.IsGranted(role, pA, nil) {
 						ctx.Next()
 						return
 					}
 				}
+
 				opt.OnNoPerm(ctx)
 				return
 			}
 
-			if roleTag := ctx.ActionTag("Role"); roleTag != "" {
+			var rolesTag string
+			if pt, ok := action.(RolesTager); ok {
+				rolesTag = pt.RolesTag()
+			} else {
+				cacheRolesLock.Lock()
+				if rolesTag, ok = cacheRoles[actionValue]; !ok {
+					rolesTag = ctx.ActionTag("Role")
+					if len(rolesTag) > 0 {
+						cacheRoles[actionValue] = rolesTag
+					}
+				}
+				cacheRolesLock.Unlock()
+			}
+
+			if len(rolesTag) > 0 {
 				roles, ok := sessions.Session(ctx.Req(), ctx.ResponseWriter).Get(opt.RoleSessionKey).([]string)
 				if !ok {
 					opt.OnNoPerm(ctx)
 					return
 				}
+
+				tag, ok, hasColon := lookup(rolesTag, ctx.Req().Method)
+				if hasColon {
+					if !ok {
+						opt.OnNoPerm(ctx)
+						return
+					}
+				} else {
+					tag = rolesTag
+				}
+
 				for _, role := range roles {
-					if roleTag == role {
+					if tag == role {
 						ctx.Next()
 						return
 					}
--- rbac_test.go
+++ rbac_test.go
@@ -80,6 +80,203 @@ func TestHasPerm(t *testing.T) {
 	expect(t, buff.String(), "You have write permission")
 }
 
+type RBACPermAction2 struct {
+	Perm `GET:"read" POST:"write"`
+}
+
+func (r *RBACPermAction2) Get() string {
+	return "You have read permission"
+}
+
+func (r *RBACPermAction2) Post() string {
+	return "You have write permission"
+}
+
+func TestNoPerm2(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	tg := tango.Classic()
+	sessions := session.New()
+	rbac := gorbac.New()
+	tg.Use(RBAC(rbac, sessions))
+	tg.Any("/perm_read_write", new(RBACPermAction2))
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/perm_read_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), DefaultNoPermString)
+
+	buff.Reset()
+
+	req, err = http.NewRequest("POST", "http://localhost:8000/perm_read_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), DefaultNoPermString)
+}
+
+func TestHasPerm2(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	tg := tango.Classic()
+	sessions := session.New()
+	rbac := gorbac.New()
+	rA := gorbac.NewStdRole("writer")
+	pRead := gorbac.NewStdPermission("read")
+	pWrite := gorbac.NewStdPermission("write")
+	rA.Assign(pRead)
+	rA.Assign(pWrite)
+	rbac.Add(rA)
+
+	if !rbac.IsGranted("writer", pWrite, nil) {
+		t.Error(errors.New("rbac setting error"))
+	}
+
+	tg.Use(tango.HandlerFunc(func(ctx *tango.Context) {
+		sess := sessions.Session(ctx.Req(), ctx.ResponseWriter)
+		sess.Set(DefaultRoleSessionKey, []string{"writer"})
+		ctx.Next()
+	}))
+
+	tg.Use(RBAC(rbac, sessions))
+	tg.Any("/perm_write", new(RBACPermAction2))
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "You have read permission")
+
+	buff.Reset()
+
+	req, err = http.NewRequest("POST", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "You have write permission")
+}
+
+type RBACPermAction3 struct {
+}
+
+func (r *RBACPermAction3) PermTag() string {
+	return `GET:"read" POST:"write"`
+}
+
+func (r *RBACPermAction3) Get() string {
+	return "You have read permission"
+}
+
+func (r *RBACPermAction3) Post() string {
+	return "You have write permission"
+}
+
+func TestNoPerm3(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	tg := tango.Classic()
+	sessions := session.New()
+	rbac := gorbac.New()
+	tg.Use(RBAC(rbac, sessions))
+	tg.Any("/perm_read_write", new(RBACPermAction3))
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/perm_read_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), DefaultNoPermString)
+
+	buff.Reset()
+
+	req, err = http.NewRequest("POST", "http://localhost:8000/perm_read_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), DefaultNoPermString)
+}
+
+func TestHasPerm3(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	tg := tango.Classic()
+	sessions := session.New()
+	rbac := gorbac.New()
+	rA := gorbac.NewStdRole("writer")
+	pRead := gorbac.NewStdPermission("read")
+	pWrite := gorbac.NewStdPermission("write")
+	rA.Assign(pRead)
+	rA.Assign(pWrite)
+	rbac.Add(rA)
+
+	if !rbac.IsGranted("writer", pWrite, nil) {
+		t.Error(errors.New("rbac setting error"))
+	}
+
+	tg.Use(tango.HandlerFunc(func(ctx *tango.Context) {
+		sess := sessions.Session(ctx.Req(), ctx.ResponseWriter)
+		sess.Set(DefaultRoleSessionKey, []string{"writer"})
+		ctx.Next()
+	}))
+
+	tg.Use(RBAC(rbac, sessions))
+	tg.Any("/perm_write", new(RBACPermAction3))
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "You have read permission")
+
+	buff.Reset()
+
+	req, err = http.NewRequest("POST", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "You have write permission")
+}
+
 type RBACRoleAction struct {
 	Role `writer`
 }
@@ -134,6 +331,177 @@ func TestHasRole(t *testing.T) {
 	tg.Any("/perm_write", new(RBACRoleAction))
 
 	req, err := http.NewRequest("GET", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "You are a writer")
+}
+
+type RBACRoleAction2 struct {
+	Role `GET:"reader" POST:"writer"`
+}
+
+func (r *RBACRoleAction2) Get() string {
+	return "You are a reader"
+}
+
+func (r *RBACRoleAction2) Post() string {
+	return "You are a writer"
+}
+
+func TestNoRole2(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	tg := tango.Classic()
+	sessions := session.New()
+	rbac := gorbac.New()
+	rReader := gorbac.NewStdRole("reader")
+	rWriter := gorbac.NewStdRole("writer")
+	rbac.Add(rReader)
+	rbac.Add(rWriter)
+
+	tg.Use(RBAC(rbac, sessions))
+	tg.Any("/perm_write", new(RBACRoleAction2))
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), DefaultNoPermString)
+}
+
+func TestHasRole2(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	tg := tango.Classic()
+	sessions := session.New()
+	rbac := gorbac.New()
+	rReader := gorbac.NewStdRole("reader")
+	rWriter := gorbac.NewStdRole("writer")
+	rbac.Add(rReader)
+	rbac.Add(rWriter)
+
+	tg.Use(tango.HandlerFunc(func(ctx *tango.Context) {
+		sess := sessions.Session(ctx.Req(), ctx.ResponseWriter)
+		sess.Set(DefaultRoleSessionKey, []string{"writer", "reader"})
+		ctx.Next()
+	}))
+
+	tg.Use(RBAC(rbac, sessions))
+	tg.Any("/perm_write", new(RBACRoleAction2))
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "You are a reader")
+
+	buff.Reset()
+
+	req, err = http.NewRequest("POST", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "You are a writer")
+}
+
+type RBACRoleAction3 struct {
+}
+
+func (r *RBACRoleAction3) RolesTag() string {
+	return `GET:"reader" POST:"writer"`
+}
+
+func (r *RBACRoleAction3) Get() string {
+	return "You are a reader"
+}
+
+func (r *RBACRoleAction3) Post() string {
+	return "You are a writer"
+}
+
+func TestNoRole3(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	tg := tango.Classic()
+	sessions := session.New()
+	rbac := gorbac.New()
+	rReader := gorbac.NewStdRole("reader")
+	rWriter := gorbac.NewStdRole("writer")
+	rbac.Add(rReader)
+	rbac.Add(rWriter)
+
+	tg.Use(RBAC(rbac, sessions))
+	tg.Any("/perm_write", new(RBACRoleAction3))
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), DefaultNoPermString)
+}
+
+func TestHasRole3(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	tg := tango.Classic()
+	sessions := session.New()
+	rbac := gorbac.New()
+	rReader := gorbac.NewStdRole("reader")
+	rWriter := gorbac.NewStdRole("writer")
+	rbac.Add(rReader)
+	rbac.Add(rWriter)
+
+	tg.Use(tango.HandlerFunc(func(ctx *tango.Context) {
+		sess := sessions.Session(ctx.Req(), ctx.ResponseWriter)
+		sess.Set(DefaultRoleSessionKey, []string{"writer", "reader"})
+		ctx.Next()
+	}))
+
+	tg.Use(RBAC(rbac, sessions))
+	tg.Any("/perm_write", new(RBACRoleAction3))
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/perm_write", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tg.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "You are a reader")
+
+	buff.Reset()
+
+	req, err = http.NewRequest("POST", "http://localhost:8000/perm_write", nil)
 	if err != nil {
 		t.Error(err)
 	}
